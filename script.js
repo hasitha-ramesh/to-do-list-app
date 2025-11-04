e757b48ba47e2a6199ed82cea5e999e577ad3cea
hasitha-ramesh <hasitha.ict@gmail.com>
2025-11-04 09:32:16 +0530

    Add task toggle and delete functionality

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const inputText = document.getElementById("inputText");
 const listContent = document.getElementById("listContent");
 
-function addText(){
-    if (inputText.value == ""){
-        alert("You must write something!");
-    }else{
-        const li = document.createElement("li");
-        li.innerHTML = inputText.value;
-        listContent.appendChild(li);
-    }
+function addText() {
+  if (inputText.value == "") {
+    alert("You must write something!");
+  } else {
+    const li = document.createElement("li");
+    li.innerHTML = inputText.value;
+    listContent.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputText.value = "";
 }
+
+listContent.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false );
